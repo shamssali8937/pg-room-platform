78291e9e2935d4441463b99338841072a8f2a291
shamssali8937 <sa4857433@gmail.com>
2026-04-12 13:35:04 +0500

    Initialize project structure with Next.js frontend and Express backend

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
