5592d644b22b19ef346f61898abf692919cd6875
shamssali8937 <sa4857433@gmail.com>
2026-04-13 18:16:29 +0500

    fxed typo

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,7 +1,9 @@
-import express from "express";
+// import express from "express";
+import { Router } from "express";
 import { signup, login } from "../controllers/auth.controller.js";
 
-const router = express.Router();
+// const router = express.Router();
+const router: Router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
