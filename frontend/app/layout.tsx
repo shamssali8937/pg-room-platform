2a87b4c7ac59f468fd6f3fddc0ead3178f179c0e
shamssali8937 <sa4857433@gmail.com>
2026-04-23 23:07:14 +0500

    updated layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       className={`${nunito.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
