e6a29dafe20b57c07eb3e3f468cea54d0d81bdc0
shamssali8937 <sa4857433@gmail.com>
2026-06-13 00:43:36 +0500

    changed typo

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/general/aboutus",
     "/general/contactus",
     "/general/privacypolicy",
-    "/general/term&conditions",
+    "/general/terms-and-conditions",
     "/auth/signin",
     "/auth/signup",
   ];
